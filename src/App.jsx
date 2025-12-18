12d1b03ed13f43844d735de1e4367009e3c712ea
Saumya1620 <saumyajain598@gmail.com>
2025-12-18 13:46:26 +0530

    updates

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react'
+import "./App.css" 
+import Navbar from './components/Navbar' 
+import Editor from '@monaco-editor/react'; 
+import Select from 'react-select'; 
+//import { GoogleGenAI } from "@google/genai"; 
+import Groq from 'groq-sdk'; 
+import Markdown from 'react-markdown' 
+import RingLoader from "react-spinners/RingLoader"; 
+const App = () => { 
+    const options = [ 
+        { value: 'javascript', label: 'JavaScript' }, 
+        { value: 'python', label: 'Python' }, 
+        { value: 'java', label: 'Java' }, 
+        { value: 'csharp', label: 'C#' }, 
+        { value: 'cpp', label: 'C++' }, 
+        { value: 'php', label: 'PHP' }, 
+        { value: 'ruby', label: 'Ruby' }, 
+        { value: 'go', label: 'Go' }, 
+        { value: 'swift', label: 'Swift' }, 
+        { value: 'kotlin', label: 'Kotlin' }, 
+        { value: 'typescript', label: 'TypeScript' }, 
+        { value: 'rust', label: 'Rust' }, 
+        { value: 'dart', label: 'Dart' }, 
+        { value: 'scala', label: 'Scala' }, 
+        { value: 'perl', label: 'Perl' }, 
+        { value: 'haskell', label: 'Haskell' }, 
+        { value: 'elixir', label: 'Elixir' }, 
+        { value: 'r', label: 'R' }, 
+        { value: 'matlab', label: 'MATLAB' }, 
+        { value: 'bash', label: 'Bash' } 
+    ]; 
+    
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+
+    const customStyles = { 
+        control: (provided) => ({ 
+            ...provided, backgroundColor: '#18181b', // dark background (similar to bg-zinc-900) 
+               borderColor: '#3f3f46', 
+               color: '#fff', width: "100%" 
+            }), 
+            menu: (provided) => ({ 
+                ...provided, backgroundColor: '#18181b', // dropdown 
+                color: '#fff', 
+                width: "100%" 
+            }), 
+            singleValue: (provided) => ({ 
+                ...provided, color: '#fff', // selected option text 
+                width: "100%" 
+            }), 
+            option: (provided, state) => ({ 
+                ...provided, 
+                backgroundColor: state.isFocused ? '#27272a' : '#18181b', // hover effect 
+                color: '#fff', 
+                cursor: 'pointer', 
+                width: "30%" 
+            }), 
+            input: (provided) => ({ 
+                ...provided, 
+                color: '#fff', 
+                width: "100%" 
+            }), 
+            placeholder: (provided) => ({ 
+                ...provided, color: '#a1a1aa', // placeholder text color 
+                width: "100%" 
+            }), 
+        }; 
+        const [code, setCode] = useState(""); 
+        //const ai = new GoogleGenAI({ apiKey: "AIzaSyDXk-ZhwXscAUAoAdIpGyUeNeZoTHgJgRw" }); 
+        const groq = new Groq({ 
+            apiKey: import.meta.env.VITE_GROQ_API_KEY, 
+            dangerouslyAllowBrowser: true 
+        }); 
+        const [loading, setLoading] = useState(false); 
+        const [response, setResponse] = useState(""); 
+        // async function reviewCode() { 
+        // // setResponse("") 
+        // // setLoading(true); 
+        // // const response = await ai.models.generateContent({ // model: "gemini-2.0-flash", // contents: You are an expert-level software developer, skilled in writing efficient, clean, and advanced code. // I’m sharing a piece of code written in ${selectedOption.value}. // Your job is to deeply review this code and provide the following: // 1️⃣ A quality rating: Better, Good, Normal, or Bad. // 2️⃣ Detailed suggestions for improvement, including best practices and advanced alternatives. // 3️⃣ A clear explanation of what the code does, step by step. // 4️⃣ A list of any potential bugs or logical errors, if found. // 5️⃣ Identification of syntax errors or runtime errors, if present. // 6️⃣ Solutions and recommendations on how to fix each identified issue. // Analyze it like a senior developer reviewing a pull request. // Code: ${code} // , // }); // setResponse(response.text) // setLoading(false); // } 
+        async function reviewCode() { 
+            setResponse(""); 
+            setLoading(true); 
+            try { 
+                const chatCompletion = await groq.chat.completions.create({ 
+                    model: "llama-3.1-8b-instant", 
+                    messages: [ 
+                        { 
+                            role: "system", 
+                            content: "You are an expert-level senior software engineer who reviews code professionally." }, 
+                            { 
+                                role: "user", 
+                                content: `
+                                    I’m sharing a piece of code written in ${selectedOption.value}.
+                                    you are a strict senior software developer. 
+                                    Your job is to deeply review this code 
+                                    Rules: 
+                                    - Code with runtime errors, undefined variables, or crashes is bad. 
+                                    - Enterprise-grade, fully safe, efficient, and clean code is excellent. 
+                                    - Works fine,but minor improvement possible is good. 
+                                    - works but unsafe, missing best practices ,Minimal structure or missing error handling is average. 
+                                    Provide the following:
+                                    1️⃣ A quality rating: Excellent, Good, Average , or Bad. 
+                                    2️⃣ Detailed suggestions for improvement. 
+                                    3️⃣ Step-by-step explanation of what the code does. 
+                                    4️⃣ Any bugs or logical errors if present. 
+                                    5️⃣ Syntax or runtime issues if present. 
+                                    6️⃣ How to fix each issue if issue is there.
+                                    
+                                    Code: 
+                                    ${code} 
+                                   `
+                                } 
+                            ] 
+                        }); 
+                        setResponse(chatCompletion.choices[0].message.content);
+        }
+                     catch (err) 
+                     { 
+                        console.error(err); 
+                        alert("Something went wrong. Check console."); 
+                    } 
+                    finally 
+                    { 
+                        setLoading(false); 
+                    } 
+                } 
+                return ( 
+                <> 
+                <Navbar /> 
+                <div className="main flex justify-between" style={{ height: "calc(100vh - 90px" }}> 
+                    <div className="left h-[87.5%] w-[50%]"> 
+                        <div className="tabs !mt-5 !px-5 !mb-3 w-full flex items-center gap-[10px]"> 
+                            <Select value={selectedOption} onChange={(e) => { setSelectedOption(e) }} options={options} styles={customStyles} /> {/* <button className="btnNormal bg-zinc-900 min-w-[120px] transition-all hover:bg-zinc-800">Fix Code</button> */} 
+                                <button onClick={() => { if (code === "") { alert("Please enter code first") } else { reviewCode() } }} className="btnNormal bg-zinc-900 min-w-[120px] transition-all hover:bg-zinc-800">Review</button> 
+                                </div> 
+                                <Editor height="100%" theme='vs-dark' language={selectedOption.value} value={code} onChange={(e) => { setCode(e) }} /> 
+                                    </div> 
+                                <div className="right overflow-scroll !p-[10px] bg-zinc-900 w-[50%] h-[101%]"> <div className="topTab border-b-[1px] border-t-[1px] border-[#27272a] flex items-center justif-between h-[60px]"> 
+                                    <p className='font-[700] text-[17px]'>Response</p> 
+                                    </div> {loading && <RingLoader color='#9333ea'/>} <Markdown>{response}</Markdown> 
+                                    </div> 
+                                    </div> 
+                                    </> 
+                                    ) 
+} 
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="nav flex items-center justify-between h-[90px] bg-zinc-900" style={{padding:"0px 150px"}}>
         <div className="logo flex items-center gap-[10px]">
           <BrainCircuit size={30} color='#9333ea'/>
-          <span className="text-2xl font-bold text-white ml-2">Codeify</span>
+          <span className="text-2xl font-bold text-white ml-2">CodeAudit</span>
         </div>
         {/* <div className="icons flex items-center gap-[20px]">
           <i className='cursor-pointer transition-all hover:text-[#9333ea]'><Sun/></i>
